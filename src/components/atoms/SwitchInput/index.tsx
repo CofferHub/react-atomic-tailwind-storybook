e2b051e89d4f76eaae02ca1a9f3ffd8c04fb6a43
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-09-26 12:50:08 -0300

    feat: Renaming some files

---
+++ src/components/atoms/SwitchInput/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SwitchInput = () => {
+	return (
+		<div>
+
+		</div>
+	);
+}
+
+export default SwitchInput;
--- src/components/atoms/TextInput/index.stories.tsx
+++ src/components/atoms/TextInput/index.stories.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import { InputProps, Input } from './index';
+import { TextInputProps, TextInput } from './index';
 
 export default {
 	title: 'Atoms/Inputs',
-	component: Input,
+	component: TextInput,
 } as Meta;
 
-const Template: Story<InputProps> = (args) => <Input {...args} />;
+const Template: Story<TextInputProps> = (args) => <TextInput {...args} />;
 
-export const TextInput = Template.bind({});
+export const BasicInput = Template.bind({});
 
-TextInput.args = {
+BasicInput.args = {
 	placeholder: 'Type a text',
 };
 
